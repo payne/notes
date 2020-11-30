fe9b6c745f109d3acef130765d19c5a989e39f4e
Matt Payne <Payne@MattPayne.org>
2020-11-29 21:33:39 -0600

    more debugging.

--- src/main/java/org/mattpayne/spring/visit/notes/controllers/WebController.java
+++ src/main/java/org/mattpayne/spring/visit/notes/controllers/WebController.java
@@ -1,5 +1,6 @@
 package org.mattpayne.spring.visit.notes.controllers;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.security.Principal;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class WebController {
@@ -55,6 +57,14 @@ public class WebController {
         System.out.println(principal);
         System.out.println(principal.getName());
         System.out.println(principal.toString());
+        System.out.println(principal.getClass().getName());
+        try {
+            OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) principal;
+            Map<String, Object> attributes = token.getPrincipal().getAttributes();
+            System.out.println(attributes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "securedPage";
     }
 
