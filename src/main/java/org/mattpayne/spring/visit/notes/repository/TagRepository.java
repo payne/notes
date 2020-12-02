9bff636663b9bd40b5ffe0063f1d86838dcfe310
Matt Payne <Payne@MattPayne.org>
2020-12-01 21:11:54 -0600

    Be able to find tags by their name.

--- src/main/java/org/mattpayne/spring/visit/notes/repository/TagRepository.java
+++ src/main/java/org/mattpayne/spring/visit/notes/repository/TagRepository.java
@@ -4,6 +4,9 @@ import org.mattpayne.spring.visit.notes.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByTag(String tagStr);
 }
