2c40ac0709d6193350dab4dd6cc4b637b84e7378
Matt Payne <Payne@MattPayne.org>
2020-12-01 19:44:20 -0600

    Allow http://localhost:8080/h2-console to work.

--- src/main/java/org/mattpayne/spring/visit/notes/configs/SecurityConfiguration.java
+++ src/main/java/org/mattpayne/spring/visit/notes/configs/SecurityConfiguration.java
@@ -10,8 +10,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         boolean isLocalHost = true;
         if (isLocalHost) { // TODO: Read this from application.yml -- use spring profiles!
-            http.antMatcher("/**").authorizeRequests()
+            // https://stackoverflow.com/questions/43794721/spring-boot-h2-console-throws-403-with-spring-security-1-5-2
+            http.authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/h2-console/**").permitAll()
                     .antMatchers("/**").permitAll();
+            // https://stackoverflow.com/questions/47221582/h2-in-memory-database-console-not-opening
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
         } else {
             http
                     .antMatcher("/**").authorizeRequests()
