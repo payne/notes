3afd14805694e2afcf753b1ed9e9b610a46ce02c
Matt Payne <Payne@MattPayne.org>
2020-12-01 21:12:39 -0600

    CommandLineRunner now reads sample.data into the database.

--- src/main/java/org/mattpayne/spring/visit/notes/NotesApplication.java
+++ src/main/java/org/mattpayne/spring/visit/notes/NotesApplication.java
@@ -3,26 +3,51 @@ package org.mattpayne.spring.visit.notes;
 import org.mattpayne.spring.visit.notes.entity.Tag;
 import org.mattpayne.spring.visit.notes.entity.Url;
 import org.mattpayne.spring.visit.notes.repository.TagRepository;
+import org.mattpayne.spring.visit.notes.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class NotesApplication implements CommandLineRunner {
 
-	@Autowired
-	private TagRepository tagRepository;
+    @Autowired
+    private UrlService urlService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotesApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Tag school = new Tag("school");
-		Url mcc = new Url("http://MCCNeb.edu");
-		school.addUrl(mcc);
-		tagRepository.save(school); // TODO: Really should use a service...
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        processFileAddUrlSaveAsWeGo("src/main/resources/sample.data");
+    }
+
+    private void processFileAddUrlSaveAsWeGo(String fileName) throws IOException {
+        List<String> list = getLinesFromFile(fileName);
+        for (int i = 0; i < list.size(); i += 2) {
+            String urlStr = list.get(i);
+            List<String> tagList = new ArrayList<>();
+            for (String str: list.get(i+1).split(" ")) {
+                tagList.add(str);
+            }
+            urlService.addUrl(urlStr,tagList);
+        }
+
+    }
+
+    private List<String> getLinesFromFile(String fileName) throws IOException {
+        List<String> list = Files.readAllLines(new File(fileName).toPath(), Charset.defaultCharset());
+        list = list.stream().filter(line -> line.trim().length() > 0).collect(Collectors.toList());
+        return list;
+    }
 }
---
+++ src/main/java/org/mattpayne/spring/visit/notes/service/UrlService.java
@@ -0,0 +1,37 @@
+package org.mattpayne.spring.visit.notes.service;
+
+import org.mattpayne.spring.visit.notes.entity.Tag;
+import org.mattpayne.spring.visit.notes.entity.Url;
+import org.mattpayne.spring.visit.notes.repository.TagRepository;
+import org.mattpayne.spring.visit.notes.repository.UrlRepository;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UrlService {
+
+    private final UrlRepository urlRepository;
+    private final TagRepository tagRepository;
+
+    public UrlService(UrlRepository urlRepository, TagRepository tagRepository) {
+        this.urlRepository = urlRepository;
+        this.tagRepository = tagRepository;
+    }
+
+    @Transactional
+    public void addUrl(String urlStr, List<String> tagStrs) {
+        Url url = new Url(urlStr);
+        url = urlRepository.save(url);
+        for (String tagStr: tagStrs) {
+            Tag tag = null;
+            Optional<Tag> optional = tagRepository.findByTag(tagStr);
+            tag = optional.isPresent() ? optional.get() : new Tag(tagStr);
+            tag.addUrl(url);
+            tagRepository.save(tag);
+        }
+    }
+}
